1a2a680e34dee17afe65fb827a56dba2060177a6
Florian Magin <8415354+fmagin@users.noreply.github.com>
2021-10-31 13:15:19 +0100

    Add runTransaction{} and .functions extension methods (#20)
    * Add runTransaction{} and .functions extension methods
    
    * Make transaction snippet clearer
    
    Co-authored-by: Florian Magin <fmagin@users.noreply.github.com>

---
+++ GhidraJupyterKotlin/src/main/java/GhidraJupyterKotlin/extensions/misc/MiscExtensions.kt
@@ -0,0 +1,38 @@
+package GhidraJupyterKotlin.extensions.misc
+
+import ghidra.framework.model.UndoableDomainObject
+import ghidra.program.model.listing.FunctionIterator
+import ghidra.program.model.listing.FunctionManager
+import ghidra.program.model.listing.Program
+
+val FunctionManager.functions: FunctionIterator
+    get() = this.getFunctions(true)
+
+
+val Program.functions: FunctionIterator
+    get() = this.functionManager.getFunctions(true)
+
+
+fun UndoableDomainObject.runTransaction(description: String, transaction: () -> Unit) {
+    val transactionID: Int = this.startTransaction(description)
+    try {
+        transaction()
+        this.endTransaction(transactionID, true)
+    }
+    catch (e: Throwable) {
+        this.endTransaction(transactionID, false)
+        throw e
+    }
+}
+
+fun UndoableDomainObject.runTransaction(transaction: () -> Unit){
+    val transactionID: Int = this.startTransaction("Kotlin Lambda Transaction")
+    try {
+        transaction()
+        this.endTransaction(transactionID, true)
+    }
+    catch (e: Throwable) {
+        this.endTransaction(transactionID, false)
+        throw e
+    }
+}
